a627dea1a82caeaddd0b703a4a4e9dfb0ea7ef91
Rafael <rafavilvert@hotmai.com>
2021-07-03 22:23:11 -0300

    print Moedas OK

--- SA3.py
+++ SA3.py
@@ -14,13 +14,18 @@ for i in modedas:
        
         if (trocoFinal >= 1):
             trocoCliente = i
-            print('Seu troco é ', trocoCliente * trocoFinal)
-            print(f'Notas de R${i}: ',trocoFinal)
-            troco = troco - (trocoFinal * i)
-            if(trocoFinal > 0 and trocoFinal < 1):
-                trocoMoedas = trocoFinal
-                trocoMoedas = float(troco / i)
-                trocoCliente = i
-                print('Seu troco é ', trocoCliente * trocoMoedas)
-                print(f'Moeda de R${i}: ',trocoFinal)
-                troco = troco - (trocoCliente * trocoMoedas)
+            if(trocoCliente >= 2):
+                print('Seu troco é ', trocoCliente * trocoFinal)
+                print(f'Notas de R${i}: ',trocoFinal)
+            else:
+                print('Seu troco é ', trocoCliente * trocoFinal)
+                print(f'Moedas de R${i}: ',trocoFinal)
+        troco = troco - (trocoFinal * i)
+        
+            # if(trocoFinal > 0 and trocoFinal < 1):
+            #     trocoMoedas = trocoFinal
+            #     trocoMoedas = float(troco / i)
+            #     trocoCliente = i
+            #     print('Seu troco é ', trocoCliente * trocoMoedas)
+            #     print(f'Moeda de R${i}: ',trocoFinal)
+            #     troco = troco - (trocoCliente * trocoMoedas)
